6ff66f7fa70ecbbda93e5b150cb14e8d0905d1ab
Pat <pat@pat-laptop.(none)>
2013-11-13 01:43:01 -0500

    Question 1 completed

---
+++ proj2.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+rm -f q1 q2
+
+#==================Compilation and error checking===============================
+gcc -o q1 -pthread -Wall -Werror q1.c
+if [ $? -eq 0 ]; then
+	gcc -o q1 -lpthread -Wall q1.c
+fi
+if [ $? -eq 0 ]; then
+	echo "Error during compilation.  Uhh...crap."
+fi
+# gcc -o q2 -pthread -Wall q2.c
+# if [ $? -eq 0 ]; then
+# 	gcc -o q2 -lpthread -Wall -Werror q2.c
+# fi
+# if [ $? -eq 0 ]; then
+# 	echo "Error during compilation.  Uhh...crap."
+# fi
+#===============Run Program 1===================================================
+
+#q1.txt is the plain-text format of the results.  
+#q1_tex.txt is plain-text but with some added formatting for copying and
+#pasting straight into a LaTeX table.  
+printf "nthreads\ttotal_time\taverage_time\n" > q1.txt
+printf "\\hlineNumber of Threads & Total Time(s) & Average Time(s)\\\\\\hline\n" > q1_tex.txt
+for i in {1..19}
+do
+	./q1 $i | awk '/nthreads/ {printf "%d\t\t%.9f\t%.9f\n", $2, $5, $9}' >> q1.txt
+	./q1 $i | awk '/nthreads/ {printf "%d\t\t& %.9f\t& %.9f\\\\\\hline\n", $2, $5, $9}' >> q1_tex.txt
+done
+for i in {20..50..5}
+do
+	./q1 $i | awk '/nthreads/ {printf "%d\t\t%.9f\t%.9f\n", $2, $5, $9}' >> q1.txt
+	./q1 $i | awk '/nthreads/ {printf "%d\t\t& %.9f\t& %.9f\\\\\\hline\n", $2, $5, $9}' >> q1_tex.txt
+done
+
+
+#This makes 2 plots of the data: one is a PNG and the other is an EPS.  I used
+#the EPS in a LaTeX report, but PNG can be used anywhere so I included that one
+#as a backup.  It was no extra trouble.
+cat << __EOF | gnuplot
+set term png size 800,600 font "DejaVuSans-Bold" 
+set output "q1_plot.png"
+set title "Individual Thread Time vs. Number of Threads"
+set xlabel "Number of Threads"
+set ylabel "Average Thread Creation and Destruction Time (s)"
+set autoscale
+plot "q1.txt" using 1:3 title "Average Time" with linespoints pointtype 6 lw 5
+
+set term postscript eps size 8,6 font "DejaVuSans-Bold, 30" enhanced color
+set output "q1_plot.eps"
+plot "q1.txt" using 1:3 title "Average Time" with linespoints pointtype 6 lw 10
+__EOF
+
+
+#cat q1.txt
---
+++ q1.c
@@ -0,0 +1,83 @@
+#include <sys/time.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+/* Function prototype */
+void* thread_func(void*);
+
+/* Global vairables */
+pthread_t *threads;	// the dynamic array containing all the threads
+long nthreads;		// the number of threads total
+
+int main(int argc, char* argv[])
+{
+	//an error message in case the input is bad
+	char message[] = {"Usage:\t ./q1 <nthreads>\nwhere nthreads is an integer representing the number of threads to use (nthreads > 0)."};
+	struct timeval start, end;
+
+	/* Error checking and input sanitizing.  If there's any error messages,
+	 * we'll end prematurely.
+	 */
+	if (argc != 2)
+	{
+		printf("%s\n", message);
+		return 0;
+	}
+	nthreads = (long) strtol(argv[1], NULL, 10);
+	if (nthreads < 1)
+	{
+		printf("%s\n", message);
+		return 0;
+	}
+
+	// create room for all the threads, also error checking
+	threads = (pthread_t *)malloc(nthreads * sizeof(pthread_t));
+	if (threads == NULL)
+	{
+		printf("There was an error allocating memory for the threads. I don't know what went wrong. Try again.\n");
+		return 1;
+	}
+
+	// now to actually create all the threads
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+	int i;
+	
+	gettimeofday(&start, NULL);
+	for(i = 0; i < nthreads; i++)
+	{
+		pthread_create(&threads[i], &attr, thread_func, (void *) (intptr_t) i);
+	}	
+
+	//they're done, now time to join them
+	for(i = 0; i < nthreads; i++)
+	{
+		pthread_join(threads[i], NULL);
+	}
+	gettimeofday(&end, NULL);
+
+	//compute the timings
+	long secs, usecs;
+	double t;
+	secs = end.tv_sec - start.tv_sec;
+	usecs = end.tv_usec - start.tv_usec;
+	t = secs + ((double) usecs/1000000);
+
+	//output results
+	printf("nthreads: %ld\t total time: %.9f\t time per thread: %.9f\n",  \
+	nthreads, t, (double) t/nthreads);
+
+	//cleanup and exit
+	free(threads);
+	return 0;
+}
+
+void* thread_func(void* data)
+{
+	//long rank = (long) (intptr_t) data;
+	//printf("Hello from thread %ld of %ld\n", rank, nthreads);
+	return NULL;
+}
